f9662659b6d28efc7a3817b5fe638455569e047d
ibeensmokinchronicc <psgunman99@gmail.com>
2026-04-06 13:04:27 -0400

    Update service-worker.js

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,22 +1,2 @@
-const CACHE_NAME = "crypto-app-v1";
-
-const urlsToCache = [
-  "/",
-  "/index.html"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener("install", e=>self.skipWaiting());
+self.addEventListener("fetch",()=>{});
